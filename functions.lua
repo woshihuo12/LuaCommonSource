1685c0c0603fb903f37106be7c584cb655be7e12
woshihuo12 <253268499@qq.com>
2016-01-14 11:39:01 +0800

    simpleSM从quick copy过来 添加其他一些辅助常用小函数

---
+++ functions.lua
@@ -0,0 +1,29 @@
+--[[--
+
+提供一组常用函数，以及对 Lua 标准库的扩展
+
+]]
+
+--[[--
+
+输出格式化字符串
+
+~~~ lua
+
+printf("The value = %d", 100)
+
+~~~
+
+@param string fmt 输出格式
+@param [mixed ...] 更多参数
+
+]]
+function printf(fmt, ...)
+    print(string.format(tostring(fmt), ...))
+end
+
+function handler(obj, method)
+    return function(...)
+        return method(obj, ...)
+    end
+end
